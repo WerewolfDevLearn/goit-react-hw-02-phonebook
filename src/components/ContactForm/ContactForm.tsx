608c75059fa18d16695f3abc220e033aad1ec174
Mikhail Khodak <werewolfdevlearn@gmail.com>
2023-04-25 00:37:42 +0300

    fix types of onSubmit

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,64 +1,32 @@
-import { nanoid } from "nanoid";
 import React, { Component } from "react";
+import { Formik } from "formik";
+// Styles
 import ContactFormStl from "./ContactForm.module.css";
-
 interface IProps {
   onAddContact(name: string, number: string): void;
 }
 
-interface IState {
-  [key: string]: string;
-}
-
-export default class ContactForm extends Component<IProps, IState> {
-  state = {
-    name: "",
-    number: "",
+export default function ContactForm({ onAddContact }: IProps) {
+  const onSubmitContact = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const { person, number } = event.currentTarget;
+    onAddContact(person.value, number.value);
   };
-
-  onInputValue = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-  };
-
-  onSubmitContact = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const { name, number } = this.state;
-
-    this.props.onAddContact(name, number);
-    this.setState({ name: "", number: "" });
-  };
-
-  render() {
-    const { name, number } = this.state;
-    return (
-      <form onSubmit={e => this.onSubmitContact(e)} className={ContactFormStl.ContactForm} id={nanoid()}>
-        <label className={ContactFormStl.label}>
-          Name
-          <input
-            type='text'
-            value={name}
-            onChange={e => this.onInputValue(e)}
-            name='name'
-            id={nanoid()}
-            className={ContactFormStl.input}
-          />
-        </label>
-        <label className={ContactFormStl.label}>
-          Phone Number
-          <input
-            type='text'
-            value={number}
-            onChange={this.onInputValue}
-            name='number'
-            id={nanoid()}
-            className={ContactFormStl.input}
-          />
-        </label>
-        <button type='submit' className={ContactFormStl.buttonSubmit}>
-          Add contact
-        </button>
-      </form>
-    );
-  }
+  return (
+    // <Formik>
+    <form onSubmit={onSubmitContact} className={ContactFormStl.ContactForm}>
+      <label className={ContactFormStl.label} htmlFor='person'>
+        Name
+        <input type='text' name='person' className={ContactFormStl.input} />
+      </label>
+      <label className={ContactFormStl.label} htmlFor='number'>
+        Phone Number
+        <input type='text' name='number' className={ContactFormStl.input} />
+      </label>
+      <button type='submit' className={ContactFormStl.buttonSubmit}>
+        Add contact
+      </button>
+    </form>
+    // </Formik>
+  );
 }
